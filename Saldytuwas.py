b944ba691d7dd828d97e786c2fb21fe4f8f0b4ee
KlebonasKoncius <littlegirlannna@gmail.com>
2023-08-18 15:37:43 +0300

    Programos Struktūra

---
+++ Saldytuwas.py
@@ -0,0 +1,25 @@
+#ŠALDYTUVAS
+'''
+1 - pridėti naują produktą
+2 - papildyti produkto kiekį
+3 - ištraukti produktą nurodant kiekį
+4 - peržiūrėti produktus
+5 - ieškoti produktų
+0 - išėjimas
+'''
+saldytuvas = {}
+
+while True:
+    tekstas = input('Pasirinkite operacija: ')
+    if tekstas == '0':
+        break
+    elif tekstas == '1':
+        pass
+    elif tekstas == '2':
+        pass
+    elif tekstas == '3':
+        pass
+    elif tekstas == '4':
+        pass
+    elif tekstas == '5':
+        pass
